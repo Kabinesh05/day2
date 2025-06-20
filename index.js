a8ff1dc54bcd0e89e23ed09b5301ee6acbf29905
kabinesh05 <vskabinesh@gmail.com>
2025-06-20 09:40:44 +0530

    a

---
+++ index.js
@@ -0,0 +1,8 @@
+const num = 2.8;
+const roundedDown = Math.floor(num);
+console.log(roundedDown); // Output: 2
+
+const num2 = 2.2;
+const roundedUp = Math.ceil(num2);
+console.log(roundedUp); // Output: 3
+
